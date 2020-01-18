8debb113fbd97c3331eb329ce09bdffaa00859dc
toyamahiroyuki <toyama.2523@gmail.com>
2020-01-18 17:08:38 +0900

    [finish]4-8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
    has_many :post_images, dependent: :destroy
    has_many :post_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
+   attachment :profile_image
 end
